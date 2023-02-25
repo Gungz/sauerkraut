f74c6df9023bfcf1324c9d154a62c86d7d5587e7
Agung Sidharta So <agung_sidharta_gungz@yahoo.com>
2023-02-25 18:43:30 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
 from flask import Flask, render_template, request
 import base64
-import pickle
+#import pickle
+import yaml
 
 app = Flask(__name__)
 
@@ -15,7 +16,7 @@ def my_form():
 def my_form_post():
     try:
         data = base64.urlsafe_b64decode(request.form['text'])
-        output = pickle.loads(data)
+        output = yaml.load(data)
     except Exception as e:
         output = e
     return render_template('home.html', code=output)
