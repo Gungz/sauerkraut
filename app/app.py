ca4816bf0d816f88976ca34f62eee08c8c937cba
Agung Sidharta So <agung_sidharta_gungz@yahoo.com>
2023-02-25 18:48:18 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 from flask import Flask, render_template, request
 import base64
-import pickle
+#import pickle
 import bleach
 #import yaml
 
@@ -17,8 +17,7 @@ def my_form():
 def my_form_post():
     try:
         data = base64.urlsafe_b64decode(request.form['text'])
-        data = bleach.clean(data)
-        output = pickle.load(data)
+        output = bleach.clean(data)
     except Exception as e:
         output = e
     return render_template('home.html', code=output)
